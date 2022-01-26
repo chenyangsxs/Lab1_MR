fcf971f06d15862ea51d1846eb7ec029e031ef36
chenyangsxs <1019886897@qq.com>
2022-01-26 22:25:28 +0800

    lab1

---
+++ testwork/mr/coordinator.go
@@ -0,0 +1,325 @@
+package mr
+
+import (
+	"container/heap"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
+
+//2021-11-20
+
+//首先应该coordinator生成所有的job，然后开始server监听所有rpc
+//搞一个简单的消息队列分发任务 感应任务状态
+
+//枚举job状态
+const (
+	JOBok = iota
+	JOBundistribute
+	JOBwaiting
+	JOBtypeMap
+	JOBtypeReduce
+)
+
+//Job任务
+type Job struct {
+	JobState int
+	JobType  int
+	File     string
+	JobID    int
+	JobTime  int64
+}
+
+//master中的协程管理
+var Alldone = make(chan bool)
+var MapjobAlldone = make(chan bool)
+
+//搞一个按照工作时间储存job的堆 堆顶是最最早被分配工作的FIFO原则
+type Heap_jop []*Job
+
+func (h Heap_jop) Len() int            { return len(h) }
+func (h Heap_jop) Less(i, j int) bool  { return h[i].JobTime < h[j].JobTime }
+func (h Heap_jop) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *Heap_jop) Push(x interface{}) { *h = append(*h, x.(*Job)) }
+func (h *Heap_jop) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+//coordinator结构体
+type Coordinator struct {
+	// Your definitions here.
+	/* 存储所有的Job key是jobID */
+	JOBall_map    map[int]*Job
+	JOBheap       Heap_jop
+	JOBchan_queue chan *Job
+	Report_chan   chan *ReportArgs
+	JOBmap_num    int
+	JOBreduce_num int
+	Maplock       sync.Mutex
+	Heaplock      sync.Mutex
+}
+
+//
+//自定义函数区
+//
+//感觉没啥用
+func (c *Coordinator) Getjoballmap() map[int]*Job {
+	c.Maplock.Lock()
+	defer c.Maplock.Unlock()
+	return c.JOBall_map
+}
+
+/* 生成所有job */
+
+func (c *Coordinator) Makejob(file []string) {
+	//
+	for index, filename := range file {
+		job := Job{
+			JobState: JOBundistribute,
+			JobType:  JOBtypeMap,
+			File:     filename,
+			JobID:    index,
+			JobTime:  time.Now().Unix(), //这个时间属实没必要
+		}
+		c.JOBall_map[job.JobID] = &job
+	}
+	for i := 0; i < c.JOBreduce_num; i++ {
+		job := Job{
+			JobState: JOBundistribute,
+			JobType:  JOBtypeReduce,
+			JobID:    c.JOBmap_num + i,
+			JobTime:  time.Now().Unix(), //这个时间属实没必要
+		}
+		c.JOBall_map[job.JobID] = &job
+	}
+	fmt.Printf("map jobs %d files\n ", len(file))
+	fmt.Printf("reduce jobs %d files\n ", c.JOBreduce_num)
+}
+
+// Your code here -- RPC handlers for the worker to call.
+//
+// an example RPC handler.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
+	reply.Y = args.X + 1
+	return nil
+}
+
+//RPC
+func (c *Coordinator) Getonejob(args *ExampleArgs, reply *RequestArgs) error {
+	for {
+		select {
+		case jobArgs := <-c.JOBchan_queue:
+			//deep copy job
+			//jobArgs := <-c.JOBchan_queue
+			reply.OneJob = *jobArgs
+			reply.Mapnum = c.JOBmap_num
+			reply.Reducenum = c.JOBreduce_num
+			fmt.Printf("worker get one job %s,chan remain %d jobs\n", reply.OneJob.File, len(c.JOBchan_queue))
+			c.Addjobtoheap(jobArgs)
+			return nil
+		default:
+			select {
+			case <-Alldone:
+				//joballdone,so pass a wrong job with out of jobtype range
+				reply.OneJob = Job{JobType: 5}
+				reply.Mapnum = 0
+				reply.Reducenum = 0
+				return nil
+			default:
+				//jobchan has no job,should wait job be putted
+				time.Sleep(time.Second)
+			}
+		}
+	}
+}
+
+//RPC
+func (c *Coordinator) Postonejob(args *ReportArgs, reply *ExampleReply) error {
+	//report信息应该阻塞
+	c.Report_chan <- args
+	return nil
+}
+
+//收集report
+func (c *Coordinator) HandleReport() {
+	for {
+		select {
+		case <-Alldone:
+			fmt.Printf("all jobs done,thread Handlereport exit\n")
+		default:
+			//do nothing
+		}
+		reportargs := <-c.Report_chan
+		c.Maplock.Lock()
+		if c.JOBall_map[reportargs.JOBID].JobState == JOBwaiting {
+			c.JOBall_map[reportargs.JOBID].JobState = JOBok
+		}
+		c.Maplock.Unlock()
+		time.Sleep(time.Second)
+	}
+}
+
+//
+// start a thread that listens for RPCs from worker.go
+//
+func (c *Coordinator) server() {
+	rpc.Register(c)
+	rpc.HandleHTTP()
+	//l, e := net.Listen("tcp", ":1234")
+	sockname := coordinatorSock()
+	os.Remove(sockname)
+	l, e := net.Listen("unix", sockname)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+}
+
+//
+// main/mrcoordinator.go calls Done() periodically to find out
+// if the entire job has finished.
+//
+//重新制作了master的流程
+//用一个map管理所有任务的状态 只对其加锁
+//done()开启三个协程  分发map任务  分发reduce任务（需要等待上一个协程执行完毕） 检查超时任务
+//worker和master用两个channel通信
+//三个协程用一个channel通信
+/* 检查undistribute 任务 */
+func (c *Coordinator) Putjob(jobtype int) {
+	//这里设置成先遍历一边map 统计未分发的任务再试图去加入ch中
+	//修改map的权限只给主线程
+	if jobtype == JOBtypeMap {
+		//do nothing
+	} else {
+		//说明此协程是执行reduce任务的，需要等待map任务全部结束
+		<-MapjobAlldone
+	}
+	for {
+		finishedJobNum := 0
+		var jobshouldputted *Job
+		c.Maplock.Lock()
+		for _, job := range c.JOBall_map {
+			if job.JobType == jobtype && job.JobState == JOBundistribute {
+				jobshouldputted = job
+				break
+			} else if job.JobType == jobtype && job.JobState == JOBok {
+				finishedJobNum++
+			}
+		}
+		c.Maplock.Unlock()
+		//处理不涉及临界区的工作
+		if jobshouldputted != nil {
+			jobshouldputted.JobState = JOBwaiting
+			c.JOBchan_queue <- jobshouldputted
+			fmt.Printf("job chan has %d jobs\n", len(c.JOBchan_queue))
+			//c.Addjobtoheap(jobshouldputted)
+			//添加成功
+		} else {
+			if finishedJobNum == c.JOBmap_num && jobtype == JOBtypeMap {
+				//切换到reduce
+				close(MapjobAlldone)
+				fmt.Printf("all map jobs done,thread putjob exit\n")
+				return
+				//exit
+			}
+			if finishedJobNum == c.JOBreduce_num && jobtype == JOBtypeReduce {
+				//广播任务结束
+				close(Alldone)
+				fmt.Printf("all jobs done,thread putjob exit\n")
+				return
+				//exit
+			}
+		}
+
+	}
+
+}
+
+//
+func (c *Coordinator) Checktimeoutjob() {
+	for {
+		select {
+		case <-Alldone:
+			//exit this thread
+			fmt.Printf("all jobs done,thread Checktimeoutjob exit\n")
+			return
+		default:
+			//do nothing
+		}
+		c.Heaplock.Lock()
+		if c.JOBheap.Len() != 0 && time.Now().Unix()-c.JOBheap[0].JobTime > 20 {
+			//超时
+			c.Maplock.Lock()
+			//
+			if c.JOBall_map[c.JOBheap[0].JobID].JobState == JOBwaiting {
+				c.JOBall_map[c.JOBheap[0].JobID].JobState = JOBundistribute
+				fmt.Printf(" job %d reset\n", c.JOBheap[0].JobID)
+			} else {
+				fmt.Printf(" job %d done\n", c.JOBheap[0].JobID)
+			}
+			c.Maplock.Unlock()
+			heap.Pop(&c.JOBheap)
+			fmt.Printf("pop job,heap has %d job\n", c.JOBheap.Len())
+		}
+		c.Heaplock.Unlock()
+		time.Sleep(time.Second * 2)
+	}
+}
+
+//heap添加成员
+func (c *Coordinator) Addjobtoheap(job *Job) {
+	c.Heaplock.Lock()
+	defer c.Heaplock.Unlock()
+	job.JobTime = time.Now().Unix()
+	heap.Push(&c.JOBheap, job)
+	fmt.Printf("add job,heap has %d job\n", c.JOBheap.Len())
+}
+
+func (c *Coordinator) Done() bool {
+	ret := true
+	// Your code here.
+	go c.Putjob(JOBtypeMap)
+	go c.Putjob(JOBtypeReduce)
+	// not very elegant
+	time.Sleep(time.Second * 2)
+	//
+	go c.Checktimeoutjob()
+	//
+	go c.HandleReport()
+
+	<-Alldone
+	return ret
+}
+
+//
+// create a Coordinator.
+// main/mrcoordinator.go calls this function.
+// nReduce is the number of reduce tasks to use.
+//
+func MakeCoordinator(files []string, nReduce int) *Coordinator {
+	c := Coordinator{
+		JOBall_map:    make(map[int]*Job),
+		JOBmap_num:    len(files),
+		JOBreduce_num: nReduce,
+		JOBchan_queue: make(chan *Job, 10),
+		Report_chan:   make(chan *ReportArgs, 10),
+	}
+	heap.Init(&c.JOBheap)
+	c.Makejob(files)
+	// Your code here.
+
+	c.server()
+	return &c
+}
---
+++ testwork2/mr/worker.go
@@ -0,0 +1,202 @@
+package mr
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+import "log"
+import "net/rpc"
+import "hash/fnv"
+
+//2021-11-19
+
+//
+// Map functions return a slice of KeyValue.
+//
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+//
+type WorkerParameter struct {
+	Mapnum    int //全部的map任务数量
+	Reducenum int //全部的reduce任务数量
+	WorkJob   *Job
+}
+
+//
+// use ihash(key) % NReduce to choose the reduce
+// task number for each KeyValue emitted by Map.
+//
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
+}
+
+//添加中间文件的生成
+func (wp *WorkerParameter) DoMapWork(mapf func(string, string) []KeyValue) error {
+	//一般来说一个worker执行一个文件（对于map任务来说）
+	filename := wp.WorkJob.File
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	content, _ := ioutil.ReadAll(file)
+	file.Close()
+	kvs := mapf(filename, string(content))
+	intermediate := make([][]KeyValue, wp.Reducenum, wp.Reducenum)
+	for _, ky := range kvs {
+		index := ihash(ky.Key) % wp.Reducenum
+		intermediate[index] = append(intermediate[index], ky)
+	}
+	//将intermediate分发给相应的文件
+	//idx is reduce job ID(0 ~ n-1)
+	for idx := 0; idx < wp.Reducenum; idx++ {
+		mapoutfilename := fmt.Sprintf("mr-%d-%d", wp.WorkJob.JobID, idx)
+		//_,err = os.Stat(mapoutfilename)
+		file, err = os.Create(mapoutfilename)
+		data, _ := json.Marshal(intermediate[idx])
+		_, err = file.Write(data)
+		file.Close()
+	}
+	//report
+	wp.ReportWork()
+	return nil
+}
+
+//
+func (wp *WorkerParameter) DoReduceWork(reducef func(string, []string) string) error {
+	kvsReduce := make(map[string][]string)
+	for idx := 0; idx < wp.Mapnum; idx++ {
+		//wp.WorkJob.JobID-wp.Mapnum : because in coordinator's map ,reducejob index is not equal to its id
+		filename := fmt.Sprintf("mr-%d-%d", idx, wp.WorkJob.JobID-wp.Mapnum)
+		file, _ := os.Open(filename)
+		content, _ := ioutil.ReadAll(file)
+		file.Close()
+		kvs := make([]KeyValue, 0)
+		_ = json.Unmarshal(content, &kvs)
+		for _, kv := range kvs {
+			_, ok := kvsReduce[kv.Key]
+			if !ok {
+				kvsReduce[kv.Key] = make([]string, 0)
+			}
+			kvsReduce[kv.Key] = append(kvsReduce[kv.Key], kv.Value)
+		}
+	}
+	ReduceResult := make([]string, 0)
+	for key, val := range kvsReduce {
+		ReduceResult = append(ReduceResult, fmt.Sprintf("%v %v\n", key, reducef(key, val)))
+	}
+	outFileName := fmt.Sprintf("mr-out-%d", wp.WorkJob.JobID-wp.Mapnum)
+	ioutil.WriteFile(outFileName, []byte(strings.Join(ReduceResult, "")), 0644)
+	//report
+	wp.ReportWork()
+	return nil
+}
+
+//
+func (wp *WorkerParameter) ReportWork() {
+	//这里没有直接传送job回去，是因为害怕指针的数据被修改
+	args := ReportArgs{
+		JOBstate: JOBok,
+		JOBtype:  wp.WorkJob.JobType,
+		JOBID:    wp.WorkJob.JobID,
+	}
+	//delete job
+	reply := ExampleReply{}
+	call("Coordinator.Postonejob", &args, &reply)
+	wp.WorkJob = nil
+}
+
+//
+func (wp *WorkerParameter) Requestonejob() {
+	// declare an argument structure.
+	args := ExampleArgs{}
+	reply := RequestArgs{}
+	call("Coordinator.Getonejob", &args, &reply)
+	wp.WorkJob = &reply.OneJob
+	wp.Reducenum = reply.Reducenum
+	wp.Mapnum = reply.Mapnum
+	fmt.Printf("get jobid %d file name %s \n", wp.WorkJob.JobID, wp.WorkJob.File)
+}
+
+//修改2021-11-18
+
+//
+// main/mrworker.go calls this function.
+//
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+
+	// Your worker implementation here.
+	// uncomment to send the Example RPC to the coordinator.
+	// CallExample()
+	//should be circle
+	flag := true
+	for flag {
+		mywp := WorkerParameter{}
+		mywp.Requestonejob()
+		switch mywp.WorkJob.JobType {
+		case JOBtypeMap:
+			mywp.DoMapWork(mapf)
+		case JOBtypeReduce:
+			mywp.DoReduceWork(reducef)
+		default:
+			//wrong jobtype(set 5)
+			fmt.Printf("worker got closed signal\n")
+			return
+		}
+	}
+}
+
+//
+// example function to show how to make an RPC call to the coordinator.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+
+func CallExample() {
+
+	// declare an argument structure.
+	args := ExampleArgs{}
+
+	// fill in the argument(s).
+	args.X = 99
+
+	// declare a reply structure.
+	reply := ExampleReply{}
+
+	// send the RPC request, wait for the reply.
+	call("Coordinator.Example", &args, &reply)
+
+	// reply.Y should be 100.
+	fmt.Printf("reply.Y %v\n", reply.Y)
+}
+
+//
+// send an RPC request to the coordinator, wait for the response.
+// usually returns true.
+// returns false if something goes wrong.
+//
+func call(rpcname string, args interface{}, reply interface{}) bool {
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := coordinatorSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer c.Close()
+
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
+}
